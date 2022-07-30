9d5bea941edffaeee1a9b3c627d231d501636743
Christopher <93827718+topherchris420@users.noreply.github.com>
2022-07-29 23:12:12 -0400

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,11 +1,11 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['Congratulations on completing your Stanford education', 'Hell yeah great job!', 'Nice!:)']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const NOT_ENOUGH_LETTERS_MESSAGE = '69'
+export const WORD_NOT_FOUND_MESSAGE = 'are you writing a poem? I love it'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Hard Mode can only be enabled when you're high'
 export const HARD_MODE_DESCRIPTION =
   'Any revealed hints must be used in subsequent guesses'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
